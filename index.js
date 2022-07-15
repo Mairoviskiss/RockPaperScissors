fd26dff023683449dd2ac55e3bbe47e537f16360
Maíra Araújo <74052943+Mairoviskiss@users.noreply.github.com>
2022-07-15 17:36:27 +0100

    game working

--- index.js
+++ index.js
@@ -6,43 +6,46 @@ const possibility3 = "scissors";
 const chosen = document.querySelector('choice');
 
 
-
-
-chosen.array.forEach(chosen => chosen.addEventListener(String, (e) => {
-  //  userTyped = e.target.id;
-    userChoice.console.log(userTyped);
-    generateComputerChoice();
-    
-}));
-
-function print() {
- submission = document.getElementById('user');
- submission.console.log ('submission');
-};
-
-const handleClick = (e) => {
- const computerChoice = possibilities[Math.floor(Math.random() * possibilities.length)];
- const userChoice = document.getElementById('user');
- getResults(userChoice, computerChoice);
-}
-
 function send() {
+ var computerChoice = [Math.floor(Math.random() * 2)];
+    
+ for (i = 0; i <= 2; i++) {
+   if (computerChoice == 0) {
+    /*computerChoice == "rock";*/
+    document.getElementById("computer").innerHTML = "rock";
+   } else if (computerChoice == 1) {
+   /* computerChoice == "paper";*/
+    document.getElementById("computer").innerHTML = "paper";
+   } else{
+    /*computerChoice == "scissors";*/
+    document.getElementById("computer").innerHTML = "scissors";
+   }
+ }
+
     var userTyped = document.getElementById("user").value;
-    document.getElementById("printSubmission").innerHTML = "<strong>You</strong> chose " + userTyped + " and the <strong>Computer</strong> chose "  ;
-   // document.getElementById("printsubmission").innerHTML = "<strong>You</strong> chose " + userTyped + "and the <strong>Computer</strong> chose " + computerChoice;
-  
-   if ((user.value).toLowerCase() != possibility1.toLowerCase()) {
-    window.alert("peee");   
-   } else if ((user.value).toLowerCase() == possibility1.toLowerCase()) {
-    window.alert("ihaaa");
-} 
+   
+    if ((user.value).toLowerCase() == possibility1 && computerChoice == 0) {
+        document.getElementById("result").innerHTML = "It's a Draw"; 
+    }  else if ((user.value).toLowerCase() == possibility2 && computerChoice == 0) {
+        document.getElementById("result").innerHTML = "You Win! Paper beats rock!"; 
+    } else if ((user.value).toLowerCase() == possibility3 && computerChoice == 0) {
+        document.getElementById("result").innerHTML = "Computer Wins! Rock beats scissor!"; 
+    }  else if ((user.value).toLowerCase() == possibility1 && computerChoice == 1) {
+        document.getElementById("result").innerHTML = "Computer Wins! Paper beats rock!";  
+    } else if ((user.value).toLowerCase() == possibility2 && computerChoice == 1) {
+        document.getElementById("result").innerHTML = "It's a draw!"; 
+    }   else if ((user.value).toLowerCase() == possibility3 && computerChoice == 1) {
+        document.getElementById("result").innerHTML = "You Win! Scissors beat paper!"; 
+    } else if ((user.value).toLowerCase() == possibility1 && computerChoice == 2) {
+        document.getElementById("result").innerHTML = "You Win! Rock beats scissor!"; 
+    } else if ((user.value).toLowerCase() == possibility2 && computerChoice == 2) {
+        document.getElementById("result").innerHTML = "Computer Wins! Scissors beat paper!";  
+    } else if ((user.value).toLowerCase() == possibility3 && computerChoice == 2) {
+        document.getElementById("result").innerHTML = "It's a Draw!";
+    }
+   
 }
 
-if (computerChoice == possibilities[1]) {
-    computerChose == "rock";
-} else if (computerChoice == possibilities[2]) {
-    computerChose == "paper";
-} else{
-    computerChose == "scissors";
-}
+ 
+
 
